d4cebb4311f453332a541a1bc4ac22b0260cdb48
KMGeon <pos04167@naver.com>
2022-10-21 00:23:23 +0900

    Commit

---
+++ src/Baek/grid/lv2_9498Better.java
@@ -0,0 +1,18 @@
+package Baek.grid;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class lv2_9498Better {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int A = Integer.parseInt(br.readLine());
+
+        System.out.print((A>=90)?"A": (A>=80)? "B": (A>=70)? "C": (A>=60)? "D": "F");
+
+    }
+}
