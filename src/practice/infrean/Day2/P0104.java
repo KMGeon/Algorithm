44d664c0dfacbc09f1a5eb74a5a99e2360d7991c
KMGeon <pos04167@kakao.com>
2023-01-17 21:27:07 +0900

    stringbuilder이용

---
+++ src/practice/infrean/Day2/P0104.java
@@ -0,0 +1,29 @@
+package practice.infrean.Day2;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class P0104 {
+//    string builder로 이용하기
+
+    public ArrayList<String> solution(int n , String[] str){
+        ArrayList<String> answer = new ArrayList<>();
+         for(String x : str){
+             String temp  = new StringBuilder(x).reverse().toString();
+             answer.add(temp);
+         }
+        return answer;
+    }
+    public static void main(String[] args) {
+        P0104 T = new P0104();
+        Scanner sc = new Scanner(System.in);
+       int n = sc.nextInt();
+       String[] str = new String[n];
+       for(int i=0; i<n; i++){
+           str[i] = sc.next();
+       }
+       for(String x : T.solution(n,str)){
+           System.out.println(x);
+       }
+    }
+}
