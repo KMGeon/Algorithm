a9d422d22f62585cedc4190a43e3aba1c341dd79
KMGeon <pos04167@kakao.com>
2023-04-23 15:36:01 +0900

    코딩테스트 준비

--- src/practice/infrean/stringPractice/day2/test.java
+++
@@ -1,9 +0,0 @@
-package practice.infrean.stringPractice.day2;
-
-public class test {
-    public static void main(String[] args) {
-        String test = new StringBuilder().append("런닝맨").append("재밌다").replace(0,1,"하이").reverse().toString();
-        System.out.println(test);
-    }
-}
- 
--- src/practice/infrean/stringPractice/day3/P0110.java
+++
@@ -1,7 +0,0 @@
-package practice.infrean.stringPractice.day3;
-
-public class P0110 {
-    public static void main(String[] args) {
-
-    }
-}
