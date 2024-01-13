b8f35d87031f95b47b1f9e19477870ecb08a337b
mugeon <pos04167@kakao.com>
2024-01-13 22:14:25 +0900

    commit

--- src/practice/자바알고리즘인터뷰/선형자료구조/배열/p8_1.java
+++ src/practice/자바알고리즘인터뷰/선형자료구조/배열/p8_1.java
@@ -2,6 +2,15 @@ package practice.자바알고리즘인터뷰.선형자료구조.배열;
 
 public class p8_1 {
     public static void main(String[] args) {
+        int[] input = {1, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+        System.out.println(p8_1.solution(input));
+    }
 
+    private static int solution(int[] input) {
+        int result =0;
+
+
+
+        return result;
     }
 }
